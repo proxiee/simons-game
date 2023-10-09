f5648b62be6915332598d04b067be842b5c6ca0f
proxiee <yashwanthtirupati99@gmail.com>
2023-10-09 02:47:38 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
@@ -6,16 +5,18 @@ var started = false;
 var level = 0;
 
 $(document).keypress(function() {
+  // Start the game when a key is pressed
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
-  }else {
+  } else {
     startOver();
-}
+  }
 });
 
 $(".btn").click(function() {
+  // Handle user button clicks
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
@@ -24,27 +25,29 @@ $(".btn").click(function() {
 });
 
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        console.log("Correct");
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      playSound("wrong");
-      $("body").addClass("game-over");
+  // Check if the user's answer is correct
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("Correct");
+    // If the user has completed the current level, move to the next level
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+        nextSequence();
+      }, 1000);
     }
+  } else {
+    // If the user's answer is incorrect, end the game
+    playSound("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+  }
 }
 
-
 function nextSequence() {
+  // Generate the next sequence of colors
   userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
@@ -57,6 +60,7 @@ function nextSequence() {
 }
 
 function animatePress(currentColor) {
+  // Add animation to the pressed button
   $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
@@ -64,15 +68,17 @@ function animatePress(currentColor) {
 }
 
 function playSound(name) {
+  // Play the sound associated with a color
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
 function startOver() {
-console.log("starting Over");
+  // Reset the game to the initial state
+  console.log("starting Over");
   level = 0;
   gamePattern = [];
   started = false;
-   $("#level-title").text("Level 0");
-    nextSequence();
+  $("#level-title").text("Level 0");
+  nextSequence();
 }
